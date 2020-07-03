82671bef1df9422df943772f55d4522422778c3c
David Johnson <david@isajohnson.com>
2020-07-03 14:38:48 -0400

    Initial terraform and ansible hookup

--- 02_terraform/main.tf
+++ 02_terraform/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "local" {
+  version = "~> 1.4"
+}
+
 resource "aws_spot_instance_request" "mc_server" {
   # Amazon Linux
   ami           = "ami-09d95fab7fff3776c"
@@ -21,4 +25,13 @@ resource "aws_spot_instance_request" "mc_server" {
   root_block_device {
     volume_size = "8"
   }
+}
+
+resource "local_file" "ansible_inventory" {
+    content     = <<-EOT
+      [mc_server]
+      ${aws_spot_instance_request.mc_server.public_dns}
+    EOT
+
+    filename = "${path.module}/../03_ansible/inventory.ini"
 }
