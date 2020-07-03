04f9a3cc4b6aa3acb1593a487eb71ebd8bc06eab
David Johnson <david@isajohnson.com>
2020-07-03 16:53:39 -0400

    Handy start and maintenance scripts

--- 02_terraform/main.tf
+++ 02_terraform/main.tf
@@ -7,6 +7,30 @@ provider "local" {
   version = "~> 1.4"
 }
 
+resource "aws_security_group" "mc_server" {
+  name        = "mc_server"
+  description = "Allow Minecraft inbound traffic"
+
+  ingress {
+    description = "Minecraft from VPC"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mc_server"
+  }
+}
+
 resource "aws_spot_instance_request" "mc_server" {
   # Amazon Linux
   ami           = "ami-09d95fab7fff3776c"
@@ -15,7 +39,7 @@ resource "aws_spot_instance_request" "mc_server" {
   wait_for_fulfillment = "true"
   key_name  = "aws-spot-minecraft-secrets"
   
-  security_groups = ["ssh-only"]
+  security_groups = ["ssh-only", "mc_server"]
 
   tags = {
     Name      = "CheapWorker"
@@ -39,7 +63,8 @@ resource "local_file" "ansible_inventory" {
 resource "local_file" "connect_sh" {
     content     = <<-EOT
       #!/bin/bash
-      ssh -i ../../../aws-spot-minecraft-secrets/aws-spot-minecraft-secrets_id_rsa ec2-user@${aws_spot_instance_request.mc_server.public_dns}
+      DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+      ssh -i $DIR/../../../aws-spot-minecraft-secrets/aws-spot-minecraft-secrets_id_rsa ec2-user@${aws_spot_instance_request.mc_server.public_dns}
     EOT
 
     filename = "${path.module}/../04_admin/bin/connect.sh"
--- 04_admin/serverAdmin.md
+++ 04_admin/serverAdmin.md
@@ -3,9 +3,21 @@
 ### Start the Server
 See: https://superuser.com/questions/582427/using-minecraft-server-with-screen
 ```
+
 $ /var/lib/minecraft/start.sh
-$ screen -ls
-$ screen -r <screen_id>
+$ /var/lib/minecraft/connect.sh
 
 # Disconnect with ctrl+a,d
+```
+
+### From a Connected Screen (interactive server terminal)
+```
+# Op a user
+op Ultimadj
+
+# Stop Server
+stop
+
+# Flush Save
+save-all
 ```
